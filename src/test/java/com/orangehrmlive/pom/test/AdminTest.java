1cbdc6dfd48e83db099712a1fb4f3d2cb69fabc0
rashedul.islam <rashedmbstucse07@gmail.com>
2022-09-05 01:30:10 +0600

    Added Admin Page verify case

---
+++ src/test/java/com/orangehrmlive/pom/test/AdminTest.java
@@ -0,0 +1,42 @@
+package com.orangehrmlive.pom.test;
+
+import com.orangehrmlive.pom.basepage.BaseTest;
+import com.orangehrmlive.pom.pages.AdminPage;
+import com.orangehrmlive.pom.pages.HomePage;
+import com.orangehrmlive.pom.pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AdminTest extends BaseTest {
+    LoginPage loginPage;
+    HomePage homePage;
+    AdminPage adminPage;
+
+    @BeforeMethod
+    public void init() {
+        initialization();
+        loginPage = new LoginPage();
+        homePage = loginPage.login("Admin", "admin123");
+        adminPage = homePage.clickAdmin();
+    }
+
+    @Test(priority = 0)
+    public void checkAdminPageShouldSucceed() {
+        Assert.assertTrue(adminPage.hasAdminTitle());
+        System.out.println("Admin Title is available");
+    }
+
+    @Test(priority = 1)
+    public void hasAdminPageTitle() {
+        Assert.assertTrue(adminPage.hasAdminTitle());
+        System.out.println("Admin Title is available");
+    }
+
+    @AfterMethod
+    public void teatDown() {
+        driver.quit();
+    }
+
+}
