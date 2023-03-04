739b79fa98a2a29d1449a30307a64b3b6c3af769
rashedul.islam <rashedmbstucse07@gmail.com>
2023-03-04 21:43:21 +0600

    Added SeHomePage test case

--- src/test/java/com/ex/se/qa/team/pages/SeHomePage.java
+++ src/test/java/com/ex/se/qa/team/pages/SeHomePage.java
@@ -1,21 +1,27 @@
 package com.ex.se.qa.team.pages;
 
 import com.ex.se.qa.team.hrmbasetest.BaseTestSeQATeam;
+import com.orangehrmlive.pom.pages.AdminPage;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class SeHomePage extends BaseTestSeQATeam {
 
     @FindBy(css = "p.oxd-userdropdown-name")
-    WebElement userDropdownName;
+    WebElement userDropdownNameEle;
+
+    @FindBy(css = "a[href$='/admin/viewAdminModule']")
+    WebElement adminLinkElement;
 
     public SeHomePage() {
         PageFactory.initElements(driver, this);
     }
 
     public boolean hasUserName() {
-        return userDropdownName.isDisplayed();
+        return userDropdownNameEle.isDisplayed();
     }
 
     public String getHomePageTitle() {
@@ -28,5 +34,11 @@ public class SeHomePage extends BaseTestSeQATeam {
         }
 
         return pageTitle;
+    }
+
+    public AdminPage clickAdmin() {
+        adminLinkElement.isDisplayed();
+        adminLinkElement.click();
+        return new AdminPage();
     }
 }
