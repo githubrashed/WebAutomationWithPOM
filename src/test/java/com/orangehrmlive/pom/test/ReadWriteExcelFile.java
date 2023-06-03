d1334ee071356aa9dbb95b2d54941bbfd8906e62
rashedul.islam <rashedmbstucse07@gmail.com>
2023-06-04 00:29:43 +0600

    Data read from excel file

--- src/test/java/com/orangehrmlive/pom/test/ReadWriteExcelFile.java
+++ src/test/java/com/orangehrmlive/pom/test/ReadWriteExcelFile.java
@@ -1,14 +1,9 @@
 package com.orangehrmlive.pom.test;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
 public class ReadWriteExcelFile {
-
     String excelFilePath = ".\\resources\\userpass.xlsx";
     FileInputStream fileInputStream = new FileInputStream(excelFilePath);
     XSSFWorkbook xssfWorkbook = new XSSFWorkbook(fileInputStream);
-
-    public ReadWriteExcelFile() throws FileNotFoundException {
-    }
 }
