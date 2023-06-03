64d2cc12adcf4319464e13e192c2a09c667d8800
rashedul.islam <rashedmbstucse07@gmail.com>
2023-06-04 00:31:46 +0600

    Data read from excel file

--- src/test/java/com/orangehrmlive/pom/test/ReadWriteExcelFile.java
+++ src/test/java/com/orangehrmlive/pom/test/ReadWriteExcelFile.java
@@ -5,7 +5,7 @@ import java.io.FileInputStream;
 public class ReadWriteExcelFile {
 
     String excelFilePath = ".\\resources\\userpass.xlsx";
-    FileInputStream fileInputStream = new FileInputStream(excelFilePath);
-    XSSFWorkbook xssfWorkbook = new XSSFWorkbook(fileInputStream);
+//    FileInputStream fileInputStream = new FileInputStream(excelFilePath);
+//    XSSFWorkbook xssfWorkbook = new XSSFWorkbook(fileInputStream);
 
 }
