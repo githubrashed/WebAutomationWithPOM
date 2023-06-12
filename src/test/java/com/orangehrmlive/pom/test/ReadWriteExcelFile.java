7f3d5c9e1feb86210b22de28279e628ab1f71a83
rashedul.islam <rashedmbstucse07@gmail.com>
2023-06-13 01:29:15 +0600

    How to read data from excel file

--- src/test/java/com/orangehrmlive/pom/test/ReadWriteExcelFile.java
+++ src/test/java/com/orangehrmlive/pom/test/ReadWriteExcelFile.java
@@ -1,11 +1,17 @@
 package com.orangehrmlive.pom.test;
 
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public class ReadWriteExcelFile {
 
-    String excelFilePath = ".\\resources\\userpass.xlsx";
-//    FileInputStream fileInputStream = new FileInputStream(excelFilePath);
-//    XSSFWorkbook xssfWorkbook = new XSSFWorkbook(fileInputStream);
+    File file = new File("D:\\Official\\WebAutomationWithPOM\\src\\test\\resources\\TestData.xlsx");
+    FileInputStream fileInputStream = new FileInputStream(file);
+    XSSFWorkbook wb = new XSSFWorkbook(fileInputStream);
 
+    public ReadWriteExcelFile() throws IOException {
+    }
 }
