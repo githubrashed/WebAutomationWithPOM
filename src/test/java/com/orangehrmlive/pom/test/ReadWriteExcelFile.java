be65b8a11e57acc29ff0ccb2b5eba14ec5c6ce54
rashedul.islam <rashedmbstucse07@gmail.com>
2023-07-22 16:32:53 +0600

    read data from excel file by Iterator

--- src/test/java/com/orangehrmlive/pom/test/ReadWriteExcelFile.java
+++ src/test/java/com/orangehrmlive/pom/test/ReadWriteExcelFile.java
@@ -8,6 +8,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Iterator;
 
 public class ReadWriteExcelFile {
     public static void main(String[] args) throws IOException {
@@ -15,12 +16,13 @@ public class ReadWriteExcelFile {
         FileInputStream fileInputStream = new FileInputStream(file);
         XSSFWorkbook wb = new XSSFWorkbook(fileInputStream);
         XSSFSheet sheet = wb.getSheetAt(0); //getSheet("sheet1")
-        int rows = sheet.getLastRowNum(); //get total row
+
+        /*int rows = sheet.getLastRowNum(); //get total row
         System.out.println("Total no of Row: " + rows);
         int cols = sheet.getRow(1).getLastCellNum(); //get total row
         System.out.println("Total no of Column under a row: " + cols);
 
-        //Read row and column data
+        //Using For Loop -- Read row and column data
         for (int r = 0; r <= rows; r++) {
             XSSFRow row = sheet.getRow(r);
             for (int c = 0; c < cols; c++) {
@@ -28,7 +30,6 @@ public class ReadWriteExcelFile {
                 switch (cell.getCellType()) { //get type of Cell
                     case STRING:
                         System.out.println(cell.getStringCellValue()); //get String value from cell
-
                         break;
                     case NUMERIC:
                         System.out.println(cell.getNumericCellValue()); //get Numeric value from cell
@@ -41,8 +42,32 @@ public class ReadWriteExcelFile {
             }
             System.out.println();
 
+        }*/
+
+        //Iterating all rows by Iterator
+
+        Iterator iterator = sheet.iterator();
+        while (iterator.hasNext()) {
+            XSSFRow row = (XSSFRow) iterator.next();
+            Iterator cellIterator = row.cellIterator();
+            while (cellIterator.hasNext()) {
+                XSSFCell cell = (XSSFCell) cellIterator.next();
+                switch (cell.getCellType()) { //get type of Cell
+                    case STRING:
+                        System.out.println(cell.getStringCellValue()); //get String value from cell
+                        break;
+                    case NUMERIC:
+                        System.out.println(cell.getNumericCellValue()); //get Numeric value from cell
+                        break;
+                    case BOOLEAN:
+                        System.out.println(cell.getBooleanCellValue()); //get boolean value from cell
+                        break;
+                }
+                System.out.println(" | ");
+
+            }
         }
-
     }
-
 }
+
+
